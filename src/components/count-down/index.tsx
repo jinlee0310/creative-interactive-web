a71350b3ebb756f4c39ecb8cb63a85354da204f7
jinlee0310 <onenmore.jin@gmail.com>
2025-01-21 11:20:45 +0900

    feat: count-down에 useCanvas 적용

--- src/components/count-down/index.tsx
+++ src/components/count-down/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useCanvas } from "../../hooks";
 
 interface CanvasProps {
   canvasWidth: number;
@@ -6,22 +6,7 @@ interface CanvasProps {
 }
 
 export default function CountDown({ canvasWidth, canvasHeight }: CanvasProps) {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas?.getContext("2d");
-    if (canvas && ctx) {
-      const devicePixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
-
-      canvas.width = canvasWidth * devicePixelRatio;
-      canvas.height = canvasHeight * devicePixelRatio;
-      ctx.scale(devicePixelRatio, devicePixelRatio);
-
-      canvas.style.width = canvasWidth + "px";
-      canvas.style.height = canvasHeight + "px";
-    }
-  }, [canvasHeight, canvasWidth]);
+  const canvasRef = useCanvas({ canvasWidth, canvasHeight });
 
   return <canvas ref={canvasRef} />;
 }
