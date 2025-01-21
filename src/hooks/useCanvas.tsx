87f435c7f549a4b115e7c97043e30fbc6d1aec43
jinlee0310 <onenmore.jin@gmail.com>
2025-01-21 11:20:11 +0900

    feat: useCanvas hook 구현

---
+++ src/hooks/useCanvas.tsx
@@ -0,0 +1,29 @@
+import { useRef, useEffect } from "react";
+
+interface CanvasProps {
+  canvasWidth: number;
+  canvasHeight: number;
+}
+
+export const useCanvas = ({ canvasWidth, canvasHeight }: CanvasProps) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext("2d");
+    if (canvas && ctx) {
+      const devicePixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+      canvas.width = canvasWidth * devicePixelRatio;
+      canvas.height = canvasHeight * devicePixelRatio;
+      ctx.scale(devicePixelRatio, devicePixelRatio);
+
+      canvas.style.width = canvasWidth + "px";
+      canvas.style.height = canvasHeight + "px";
+
+      ctx.fillRect(10, 10, 50, 50);
+    }
+  }, [canvasHeight, canvasWidth]);
+
+  return canvasRef;
+};
